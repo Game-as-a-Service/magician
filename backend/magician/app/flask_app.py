31382969085053eed562123707119f23dd5b91fd
teds-lin <tedslin@gmail.com>
2023-07-18 22:13:53 +0800

    [API]add CORS support

--- backend/magician/app/flask_app.py
+++ backend/magician/app/flask_app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from flask_cors import CORS
 from magician.service.print_hello import print_hello
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/hello', methods=['GET'])
 def api_hello():
