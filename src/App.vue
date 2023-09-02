dd7488219089d5fc7c30b4bf1ba955c2958f53d6
leave3310 <sampisam3@gmail.com>
2023-09-02 17:21:15 +0800

    [功能] 引入HintBar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
+    <HintBar
+      :hint-text="'輪到你了！ 請選擇魔法！'"
+      :change-color="'bg-purple text-white'"
+    ></HintBar>
+    <HintBar
+      :hint-text="'遊戲開始'"
+      :change-color="'bg-skin'"
+    ></HintBar>
     <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
-@use '@/assets/styles/self/index.scss'
+<script setup>
+import HintBar from '@/components/common/HintBar.vue'
+</script>
+
+<style>
+body{
+  background-color: black;
+}
 </style>
