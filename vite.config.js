ff768f8440a433c1049c8321a53455112ed5033b
leave3310 <sampisam3@gmail.com>
2023-09-02 17:20:14 +0800

    [功能] 移除scss相關設定

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,7 @@ export default defineConfig({
     // outDir: resolve('dist', env.OUTPUT_DIR)
   },
   css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
-        additionalData: '@use "@/assets/styles/abstract/_index.scss" as *;'
-      }
-    }
+    
   },
   plugins: [ vue() ],
   resolve: {
