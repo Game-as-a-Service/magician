bc9d4f6a993194ac39f6390b2a311f63de92d57b
leave3310 <sampisam3@gmail.com>
2023-10-15 21:10:01 +0800

    feat: 新增在 tailwind 上的色票

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 export default {
   content: [ './index.html', './src/**/*.{vue,js,ts,jsx,tsx}' ],
   theme: {
-    colors: {
-      // Configure your color palette here
-      red: '#AE0000',
-      'red-dark': '#730000',
-      pink: '#EC7DD3',
-      purple: '#8962F8',
-      skyblue: '#7195F2',
-      grassgreen: '#68C907',
-      yellow: '#EACA24',
-      'yellow-other': '#FFE55A',
-      'yellow-bright': '#FEE718',
-      orange: '#FF8D4D',
-      white: '#F7F7F7',
-      grey: '#8C8C8C',
-      grey50: '#8C8C8C',
-      skin: '#FFFFFF',
-      'white20%': 'rgba(255, 255, 255, 0.20)',
-      oldBook: '#C8B59F',
+    extend: {
+      colors: {
+        // Configure your color palette here
+        red: '#AE0000',
+        'red-dark': '#730000',
+        pink: '#EC7DD3',
+        purple: '#8962F8',
+        skyblue: '#7195F2',
+        grassgreen: '#68C907',
+        yellow: '#EACA24',
+        'yellow-other': '#FFE55A',
+        'yellow-bright': '#FEE718',
+        orange: '#FF8D4D',
+        white: '#F7F7F7',
+        grey: '#8C8C8C',
+        grey50: '#8C8C8C',
+        skin: '#FFFFFF',
+        'white20%': 'rgba(255, 255, 255, 0.20)',
+        oldBook: '#C8B59F',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
