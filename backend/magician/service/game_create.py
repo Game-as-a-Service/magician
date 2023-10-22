212151990311eb432c43cef4dc3f4e2db304a61a
yock wang <ctjh900810@hotmail.com>
2023-10-22 15:50:42 +0800

    fix e2e test_api & game_create

--- backend/magician/service/game_create.py
+++ backend/magician/service/game_create.py
@@ -4,7 +4,7 @@ from magician.repository.playerclass import Player_repository
 
 
 def game_create(players):
-    players_number = len(players["playerIDs"])
+    players_number = len(players)
     if players_number <= 3:
         return "players is not enough"
     elif players_number >= 6:
@@ -13,13 +13,24 @@ def game_create(players):
         except_input_seat = list(range(players_number))
         except_input_HP = [6]*players_number
         except_input_score = [0]*players_number
-        Player_repository(
-            players["playerIDs"],
-            except_input_seat,
-            except_input_HP,
-            except_input_score
-        )
-        create_room_id(players["playerIDs"][0])
+        
+        for player in players:
+            player_name = Player_repository(player,players)
+            player_name.set_player_name()
+
+        for player,seat in zip(players,except_input_seat):
+            player_seat = Player_repository(player)
+            player_seat.set_players_seat(seat)
+
+        for player,hp in zip(players,except_input_HP):
+            player_hp = Player_repository(player)
+            player_hp.set_players_hp(hp)
+
+        for player,score in zip(players,except_input_score):
+            player_score = Player_repository(player)
+            player_score.set_players_score(score)
+        
+        create_room_id(players[0])
         #round_start()
         return "start game"
 
--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -11,32 +11,25 @@ def test_hello(app, client):
 # TODO遊戲人數5人
 # 開始遊戲
 def test_game_create_success(app, client):
-    expected = {"playerIDs": ["Teds", "Tux", "Yock", "Momo", "Leave3310"]}
-    # expected = {'playerIDs': [
-    # {"id":"Teds"},
-    # {"id":"Tux"},
-    # {"id":"Yock"},
-    # {"id":"Momo"},
-    # {"id":"Leave3310"},
-    # ]}
+    expected = ["Teds", "Tux", "Yock", "Momo", "Leave3310"]
     res = client.post("/gameCreate", json=expected)
     assert res.status_code == 201
 
 
 def test_game_create_is_less_than_4_players(app, client):
-    expected = {"playerIDs": ["Teds", "Tux", "Yock"]}
+    expected =  ["Teds", "Tux", "Yock"]
     res = client.post("/gameCreate", json=expected)
     assert res.status_code == 400
 
 
 def test_game_create_more_than_5_players(app, client):
-    expected = {"playerIDs": ["Teds", "Tux", "Yock", "Momo", "Leave3310", "dn"]}
+    expected = ["Teds", "Tux", "Yock", "Momo", "Leave3310", "dn"]
     res = client.post("/gameCreate", json=expected)
     assert res.status_code == 400
 
 
 def test_gameroom_create(app, client):
-    expected = {"playerIDs": ["Teds", "Tux", "Yock", "Momo", "Leave3310"]}
+    expected = ["Teds", "Tux", "Yock", "Momo", "Leave3310"]
     res = client.post("/gameCreate", json=expected)
     assert res.status_code == 201
     res = client.put("/player/Teds/join")
