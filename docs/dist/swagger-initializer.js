efe6cf821d5678068984d3b7aac560b240257f18
Teds <125881849+teds-lin@users.noreply.github.com>
2023-11-22 08:47:37 +0800

    [Refactor] API Docs: 改為用相對路徑載入yaml檔

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/Game-as-a-Service/magician/backend_teds/docs/openapi.yaml",
+    url: "openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
