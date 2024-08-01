ac74452764c321dead38c1b31be50b483cce9847
Parsons <gongyingparsons@gmail.com>
2024-08-02 00:19:56 +0800

    lint

--- src/components/PlayDice.vue
+++ src/components/PlayDice.vue
@@ -128,33 +128,43 @@ onMounted(() => {
   0% {
     translate: 200% -300%;
   }
-  10%{
+
+  10% {
     translate: 180% -240%;
   }
-  20%{
+
+  20% {
     translate: 160% -150%;
   }
-  30%{
+
+  30% {
     translate: 140% -80%;
   }
-  40%{
+
+  40% {
     translate: 120% 0%;
   }
-  50%{
+
+  50% {
     translate: 100% -40%;
   }
-  60%{
+
+  60% {
     translate: 80% -50%;
   }
-  70%{
+
+  70% {
     translate: 60% -40%;
   }
-  80%{
+
+  80% {
     translate: 40% 0;
   }
-  90%{
+
+  90% {
     translate: 20% -20%;
   }
+
   100% {
     translate: 0% 0;
   }
